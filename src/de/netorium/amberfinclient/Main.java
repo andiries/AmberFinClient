21b776c9c6e357dc8afe12cea79b440d88c55753
Andreas Ries <andreas.ries@netorium.de>
2016-05-10 17:53:49 +0200

    First contact to amberfin test server

--- src/de/netorium/amberfinclient/Main.java
+++ src/de/netorium/amberfinclient/Main.java
@@ -4,22 +4,26 @@ import com.amberfin.ws.dark._2014._07.transcode.TranscodeWS;
 import com.amberfin.ws.dark._2014._07.transcode.TranscodeWSException_Exception;
 import com.amberfin.ws.dark._2014._07.transcode.TranscodeWSService;
 
-import javax.xml.ws.BindingProvider;
+import javax.xml.namespace.QName;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Main {
 
-    public static void main(String[] args) {
-        TranscodeWSService amberfinService = new TranscodeWSService();
-        TranscodeWS amberfin = amberfinService.getTranscodeWSPort();
+    public static void main(String[] args) throws MalformedURLException {
 
-        String endpointURL = "http://NEW_ENDPOINT_URL";
-        BindingProvider bindingProvider = (BindingProvider)amberfin;
-        bindingProvider.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, endpointURL);
+        URL endpoint = new URL("http://10.10.10.109:21012/dark/ws/Transcode?wsdl");
+        QName qname = new QName("http://amberfin.com/ws/dark/2014/07/Transcode", "TranscodeWSService");
+
+        TranscodeWSService amberfinService = new TranscodeWSService(endpoint, qname);
+        TranscodeWS amberfin = amberfinService.getTranscodeWSPort();
 
         int progress;
         try {
             progress = amberfin.getJobProgress("");
         } catch (TranscodeWSException_Exception e) {
+            e.printStackTrace();
+        } catch (Exception e)  {
             e.printStackTrace();
         }
 
